2e4d76f7f4469e71762126fd0596c4972585851c
Joxing <44520847+junxingzhi@users.noreply.github.com>
2019-12-26 14:29:49 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "骏行の博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "骏行の"
+email = "joxing@outlook.com"
 author_homepage = "https://www.imalan.cn"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['Maverick', '骏行の', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
